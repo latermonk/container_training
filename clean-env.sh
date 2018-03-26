3cf15a0faf231b6ee9e89f0665c3b1b950ec05f2
latermonk <iback.china@gmail.com>
2018-03-26 08:47:24 +0800

    Create clean-env.sh

---
+++ clean-env.sh
@@ -0,0 +1,14 @@
+ #!/bin/bash
+ kubectl get node
+ kubectl get pod
+ kubectl get deployment
+ kubectl get rc
+ kubectl get svc
+ 
+ 
+
+ kubectl delete deployment  hello-baby
+ kubectl delete svc  hello-baby
+ kubectl delete pod  hello-baby-5b6b899ff9-j7g89
+ kubectl delete rc   xxx
+ 
